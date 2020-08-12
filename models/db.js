c00195ed10e3da11651d6c1392ee7b4283e1c084
harikewalkushwaha <harikewalkushwaha65@gmail.com>
2020-08-12 19:41:27 +0530

    Add files via upload

---
+++ models/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/gambo', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) throw err;
+    console.log("Connection setup successfully");
+});
+
+require('./banner')
+require('./conatctus')
+require('./signup')
+require('./addproduct')
+require('./addarea')
+require('./addlocation')
+require('./category')
+require('./addshop')
+require('./offer')
+require('./order')
+require('./cart1')
+require('./settings')
+require('./posts')
+require('./postcategory')
+require('./post_tags')
+require('./reports')
